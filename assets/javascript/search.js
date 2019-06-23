d63a1df7d0fc9fcea12085f99b83b117a9185062
Joby <janthonywolfe@gmail.com>
2019-06-23 11:11:55 -0600

    added still and play function

--- assets/javascript/search.js
+++ assets/javascript/search.js
@@ -1,14 +1,14 @@
-let places = ['USA','Canada','France','China','England','Russia'];
+let topics = ['USA','Canada','France','China','England','Russia'];
 
-places.every(element => {
-    for (i=0;i<places.length;i++) {
-        $('#buttons').append('<button data-location=' + places[i] + '>' + places[i] + '</button>');    
+topics.every(element => {
+    for (i=0;i<topics.length;i++) {
+        $('#buttons').append('<button data-location=' + topics[i] + '>' + topics[i] + '</button>');    
     }
 })
 
 let buttonClick = $('button').on('click', function () {
     let location = $(this).attr('data-location');
-    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + location + '&api_key=CHI9LcOr4nNtjXbqGSfFsNSG6Q8BH8cm&limit=5';
+    let queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + location + '&api_key=CHI9LcOr4nNtjXbqGSfFsNSG6Q8BH8cm&limit=10';
     $.ajax({
         url: queryURL,
         method: 'GET',
@@ -17,16 +17,37 @@ let buttonClick = $('button').on('click', function () {
         let results = response.data;
         console.log(results);
         
-        for(let j=0; j < 5; j++) {
+        for(let j=0; j < 10; j++) {
         let gifDiv = $('<div>');
         let rating = results[j].rating;
-        let ratingText = $('<p>').text('rating : ' + rating);
-        let gifImg = $('<img>');
+        let ratingText = $('<p>').text('Rating: ' + rating);
+        
+        gifImg = $('<img>');
+        // gifImg.attr()
+        gifImg.attr('src', results[j].images.fixed_height_still.url);
+        gifImg.attr('data-still', results[j].images.fixed_height_still.url);
+        gifImg.attr('data-animate', results[j].images.fixed_height.url);
+        gifImg.attr('data-state', 'still');
+        gifImg.on('click', aniGif);
+        
 
-        gifImg.attr('src', results[j].images.fixed_height.url);
-        gifDiv.append(ratingText);
         gifDiv.append(gifImg);
-        $('#gifDiv').append(gifDiv);
+        gifDiv.append(ratingText);
+        $('#gifDiv').prepend(gifDiv);
         }
     })
 })
+function aniGif() {
+    let gifStop = $(this).attr('data-still');
+    let gifPlay = $(this).attr('data-animate');
+    let gifState = $(this).attr('data-state');
+    if (gifState === 'still'){
+        $(this).attr("src", gifPlay);
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", gifStop);
+        $(this).attr("data-state", "still");
+    }
+    
+    console.log(this);
+}
